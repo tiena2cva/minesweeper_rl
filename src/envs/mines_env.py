c049fe883c54084a26ae6c7654fa12b9e3a4408d
Viet-Tien Ngoc <tiena2cva@gmail.com>
2019-11-25 17:30:06 +0700

    init env

---
+++ src/envs/mines_env.py
@@ -0,0 +1,17 @@
+from gym import Wrapper, Env
+
+
+class MinesEnv(Env):
+    def __init__(self):
+        pass
+
+    # OpenAI gym API
+
+    def step(self, action):
+        pass
+
+    def reset(self):
+        pass
+
+    def render(self, mode='human'):
+        pass
